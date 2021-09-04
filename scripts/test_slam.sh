1476bdf268d1e35958333323af05efcb0e0c662e
webmiraclepro <webmiraclepro@gmail.com>
2021-09-04 15:44:05 +0800

    Initial commit: Created scripts to to manually map my world.

---
+++ scripts/test_slam.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+xterm -e " source ~/catkin_ws/devel/setup.bash; export ROBOT_INITIAL_POSE=\"-x 1.0 -y -1.0 -z 0.12\"; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/robond/catkin_ws/src/map/MyHouse.world " &
+sleep 5
+xterm -e " source ~/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo gmapping_demo.launch " &
+sleep 5
+xterm -e " source ~/catkin_ws/devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch " &
+sleep 5
+xterm -e " source ~/catkin_ws/devel/setup.bash; roslaunch turtlebot_teleop keyboard_teleop.launch "
