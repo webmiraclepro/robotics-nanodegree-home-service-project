3249aff463196bd9ff0ea6597f43f301c8e022a1
webmiraclepro <webmiraclepro@gmail.com>
2021-09-16 04:41:42 +0800

    Add the map to the test_navigation and pick_objects scripts. Fix pick_objects script to use launch file

--- scripts/pick_objects.sh
+++ scripts/pick_objects.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 xterm -e " source devel/setup.bash; export ROBOT_INITIAL_POSE=\"-x 1.0 -y -1.0 -z 0.12\"; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(pwd)/src/map/MyHouse.world " &
 sleep 5
-xterm -e " source devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch " &
+xterm -e " source devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(pwd)/src/map/map.yaml initial_pose_x:=2 initial_pose_y:=-1 initial_pose_a:=0.785" &
 sleep 5
 xterm -e " source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch " &
 sleep 5
-xterm -e " source devel/setup.bash; rosrun pick_objects pick_objects "
+xterm -e " source devel/setup.bash; source devel/setup.bash; roslaunch pick_objects pick_objects.launch "
